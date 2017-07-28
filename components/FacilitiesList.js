d3ee8496b61e84f7f09e26153da04e1a82ca38cf
JohnHBurke <johnburke2015@yahoo.com>
2017-07-28 13:38:46 -0400

    Add files via upload

--- components/FacilitiesList.js
+++ components/FacilitiesList.js
@@ -1,114 +1,55 @@
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView, Dimensions, TouchableOpacity } from 'react-native';
-const faciltiesArray = ['a', 'b', 'c', 'd']
 
 
 export default class App extends React.Component {
 
 
 
-  facilitySelected() {
+    constructor(props) {
+	super(props)
+	this.state = {
+		allBuildingNames: ['thorton']
+	}
+}
+
+  buildingSelected(building) { console.log( building)
       fetch(
-        "138.197.11.189:3000/api/AIzaSyAroiIgW9Qyn2QOMvdjJxZqntnDkdNH8Fc/buildings"
-      ).then(results => {
+        "http://138.197.11.189:3000/5901ffbfd786a21a6b69eb9a/sensors/" + building
+      ). if(results => {
         results.json().then(json => {
-          console.log(json)
-          this.setState({ allBuildingData: json });
-          this.props.navigation.navigate('FacilitiesDemand', { categoryBuildingData: json} )
-        })
+          { console.log( json)}
+        });
       });
-    }
-
-    componentWillMount() {
-          fetch(
-            "http://138.197.11.189:3000/5900e4e9ddb6a40f274b7361/buildings"
-          ).then(results => {
-            results.json().then(json => {
-              console.log(json)
-              this.setState({ allBuildingData: json });
-              this.props.navigation.navigate('FacilitiesDemand', { categoryBuildingData: json} )
-            })
-          });
-        }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <ScrollView>
-          <TouchableOpacity>
-            <View style={styles.options}>
-              <Text style={styles.optionsText}>
-                Facility ZY1
-              </Text>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <View style={styles.options}>
-              <Text style={styles.optionsText}>
-                Facility X
-              </Text>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <View style={styles.options}>
-              <Text style={styles.optionsText}>
-                Facility X
-              </Text>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <View style={styles.options}>
-              <Text style={styles.optionsText}>
-                Facility X
-              </Text>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <View style={styles.options}>
-              <Text style={styles.optionsText}>
-                Facility X
-              </Text>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <View style={styles.options}>
-              <Text style={styles.optionsText}>
-                Facility X
-              </Text>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <View style={styles.options}>
-              <Text style={styles.optionsText}>
-                Facility X
-              </Text>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <View style={styles.options}>
-              <Text style={styles.optionsText}>
-                Facility X
-              </Text>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <View style={styles.options}>
-              <Text style={styles.optionsText}>
-                Facility X
-              </Text>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <View style={styles.options}>
-              <Text style={styles.optionsText}>
-                Facility X
-              </Text>
-            </View>
-          </TouchableOpacity>
-        </ScrollView>
-      </View>
-    );
   }
+
+
+  componentWillMount() {
+    fetch(
+    "http://138.197.11.189:3000/5901ffbfd786a21a6b69eb9a/buildings"
+  ).then(results => {
+    results.json().then(json => {
+      this.setState({ allBuildingNames: json });
+    });
+  });
+ }
+       render() {
+       return (
+       <View style={styles.container}>
+         <ScrollView>
+           {this.state.allBuildingNames.map(building =>
+             <TouchableOpacity style={styles.options}
+             onPress={this.buildingSelected.bind(this, building.Name)}>
+               <Text style={{marginLeft: 5, color: 'black', fontSize: 16}}>
+                 {building.Name}
+               </Text>
+             </TouchableOpacity>
+           )}
+         </ScrollView>
+       </View>
+       )
+      }
+
 }
 
 const styles = StyleSheet.create({
